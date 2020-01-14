30d5828e6dbdd0d76d0f3479ef1d427aa74a77ef
Kemoy Joseph <josephkemoy@gmail.com>
2020-01-14 18:02:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 function dwarfRollCall(dwarves) {
   var ar = []
-  var i = 0
-  var x = 0
+  var i = (dwarves.length/2)
+  var x = (dwarves.length/2)
   var res = ""
   
   while(i<dwarves.length){
     ar.push(""+[i+1]+". "+dwarves[i]+" ")
     i++
   }
+  
   
   while(x<dwarves.length){
     res = res.concat(ar[x])
@@ -52,14 +53,28 @@ function findTheCheese (foods) {
   var test
   
   while(i<foods.length){
-    if(foods[i] == "cheddar" || foods[i] == "gouda" || foods[i] == "camembert"){
+    if(foods[i] == "cheddar" || foods[i] == "gouda" || foods[i] == "camembert" || foods[i] == "swiss cheese"){
       test = foods[i]
     }
     
     i++
   }
   
-  if(test == "cheddar" || test == "gouda" || test == "camembert"){
+  if(test == "cheddar" || test == "gouda" || test == "camembert" || foods[i] == "swiss cheese"){
     return test
   } else return "no cheese!"
+}
+
+
+function wordWithB(words){
+  var ar = []
+
+  
+  for(let i = 0; i<words.length;i++){
+    if(words[i].charAt(0) == "b" || words[i].charAt(0) == "B"){
+      ar.push(words[i])
+    }
+  }
+  
+  
 }
